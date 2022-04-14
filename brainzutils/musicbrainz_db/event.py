be1d7ddec3592b1a926ea71cae300495a99c5062
Ansh Goyal <anshg1214@gmail.com>
2022-04-14 09:12:54 +0000

    agaiinn

--- brainzutils/musicbrainz_db/event.py
+++ brainzutils/musicbrainz_db/event.py
@@ -95,32 +95,27 @@ def fetch_multiple_events(mbids, includes=None):
 
         return {str(mbid): serialize_events(event, includes_data[event.id]) for mbid, event in events.items()}
 
-def get_event_for_place(place_id, limit=None, offset=None):
+def get_event_for_place(place_id, event_types, limit=None, offset=None):
     """Get all events linked to a place.
-
     Args:
         place_id (uuid): MBID of the place.
         limit (int): Max number of events to return.
         offset (int): Offset that can be used in conjunction with the limit.
-
     Returns:
         A tuple containing a dictionary containing info of multiple events keyed by their MBID, and the total number of events.
     """
 
     place_id = str(place_id)
     with mb_session() as db:
-        event_query = db.query(models.Event).\
+        event_query = db.query(models.Event.gid).join(models.EventType).\
             join(models.LinkEventPlace, models.Event.id == models.LinkEventPlace.entity0_id).\
             join(models.Place, models.LinkEventPlace.entity1_id == models.Place.id).\
-            filter(models.Place.gid == place_id)
-        
+            filter(models.Place.gid == place_id).filter(models.EventType.in_(event_types)).\
+            order_by(models.Event.begin_date_year.desc())
+
         count = event_query.count()
-        # events = event_query.order_by(models.Event.begin_date_year.desc()).\
-        # limit(limit).offset(offset).all()
         events = event_query.limit(limit).offset(offset).all()
-        
-        # event_ids = [event[0] for event in events]
-    
-    # event = fetch_multiple_events(event_ids)
-    events = ([serialize_events(event) for event in events], count)
-    return events, count
+        event_ids = [event[0] for event in events]
+
+    event = fetch_multiple_events(event_ids)
+    return event, count
